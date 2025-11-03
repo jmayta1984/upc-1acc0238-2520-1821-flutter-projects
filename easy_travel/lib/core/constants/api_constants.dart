16696150ebb19b559bbf2a2e48eca4b7b92c30ac
jmayta1984 <jmayta@pucp.edu.pe>
2025-11-03 11:59:08 -0500

    feat(core): add api constants

--- easy_travel/lib/core/constants/api_constants.dart
+++ easy_travel/lib/core/constants/api_constants.dart
@@ -2,4 +2,6 @@ class ApiConstants {
   static const String baseUrl =
       'https://destinationapp-h4e8dvace3fqffbb.eastus-01.azurewebsites.net';
   static const String destinationsEndpoint = '/api/destinations';
+  static const String loginEndpoint = 'api/users/login';
+  static const String registerEndpoint = 'api/users/register';
 }
