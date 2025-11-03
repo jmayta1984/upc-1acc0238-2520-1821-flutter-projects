092ba6cd599c65848545ce4e6bb51ec23c996bd0
jmayta1984 <jmayta@pucp.edu.pe>
2025-11-03 11:58:24 -0500

    feat(home): add enum for categories

--- easy_travel/lib/features/home/presentation/blocs/home_state.dart
+++ easy_travel/lib/features/home/presentation/blocs/home_state.dart
@@ -1,6 +1,6 @@
+import 'package:easy_travel/core/enums/status.dart';
 import 'package:easy_travel/features/home/domain/destination.dart';
 
-enum Status { initial, loading, success, failure }
 
 class HomeState {
   final Status status;
